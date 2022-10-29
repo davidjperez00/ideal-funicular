8a91bfbd3bc0fafeac68b01aad68f185da1044da
[David Perez] <david.j.perez00@gmail.com>
2022-10-28 21:48:33 -0500

    Import base Xcode project
    Hello world base application using Xcode IDE

---
+++ iphone-UI/Shared/iphone_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iphone_UIApp.swift
+//  Shared
+//
+//  Created by David Perez on 10/28/22.
+//
+
+import SwiftUI
+
+@main
+struct iphone_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
