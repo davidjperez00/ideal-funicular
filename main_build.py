e74dc87f47d0d9ba4dd08d2fc40456ab2df9b2fa
[David Perez] <david.j.perez00@gmail.com>
2022-12-26 18:32:05 -0600

    Add new model testing an training

--- main_build.py
+++ main_build.py
@@ -9,13 +9,12 @@
 import cv2
 import os
 import numpy as np
-from tensorflow import keras
 import tensorflow as tf
 import math
 import matplotlib.pyplot as plt
+from tensorflow import keras
 import sys
 sys.path.append("/multiclass_MLP_v1_alternate")
-
 
 
 # Global varibles
@@ -33,8 +32,46 @@ class ExponentialLearningRate(keras.callbacks.Callback):
         self.losses.append(logs["loss"])
         K.set_value(self.model.optimizer.learning_rate, self.model.optimizer.learning_rate * self.factor)
 
+def load_add_operator_dataset():
 
-# previous implementation, most recent was V2 on 12/25/2022
+  images_names = os.listdir("./datasets/full_+_preprocessed/")
+  add_images = []
+  for img_name in images_names:
+    image = cv2.imread('./datasets/full_+_preprocessed/'+ img_name, 0)
+
+    add_images.append(image)
+
+  # Convert image to numpy array to match MNIST datatype
+  images = np.asarray(add_images)
+
+  # Load labels
+  image_labels = create_add_operator_labels()
+
+  # Convert labels to numpy array to match MNIST datatype
+  image_labels = np.asanyarray(image_labels)
+
+  # Remove 14% for test
+  # X_valid, X_train = X_train_full[:5000] / 255., X_train_full[5000:] / 255.
+  labels_frac = math.ceil(len(image_labels) * (1/7)) # >>> 604
+  images_test, images_train = images[:labels_frac] / 255., images[labels_frac:] / 255. 
+  labels_test, labels_train = image_labels[:labels_frac], image_labels[labels_frac:]
+
+  # Take another 8% for validation set, remainder stays in train
+  labels_frac = math.ceil(len(image_labels) * (.5/6)) # >>> 353
+  images_valid, images_train = images[:labels_frac] / 255., images[labels_frac:] / 255. 
+  labels_valid, labels_train = image_labels[:labels_frac], image_labels[labels_frac:] 
+
+  return images_train, images_valid, images_test, labels_train, labels_valid, labels_test
+
+
+# Create float labels for every item in directory
+def create_add_operator_labels():
+  preprocessed_image_names = os.listdir("./datasets/full_+_preprocessed/")
+  add_labels = 10
+  y_add_labels = [add_labels for img in range(len(preprocessed_image_names))]
+
+  return np.asarray(y_add_labels)
+
 # @brief Generate data subsets for neural net model.
 def create_mnist_train_test():
   # number of images, 28x28 pixels
@@ -46,7 +83,32 @@ def create_mnist_train_test():
   y_valid, y_train = y_train_full[:5000], y_train_full[5000:]
   X_test = X_test / 255.
 
-  return X_train,  X_valid, X_test, y_train, y_valid, y_test
+  # Get add operator dataset with labels
+  add_train, add_valid, add_test, y_add_train, y_add_valid, y_add_test = load_add_operator_dataset()
+
+  # Add add images to MNIST 
+  X_train_new = np.concatenate((X_train, add_train))
+  X_test_new = np.concatenate((X_test, add_test))
+  X_valid_new = np.concatenate((X_valid, add_valid))
+  del add_train, add_valid, add_test
+
+  y_train_new = np.concatenate((y_train, y_add_train))
+  y_valid_new = np.concatenate((y_valid, y_add_valid))
+  y_test_new = np.concatenate((y_test, y_add_test))
+  del y_add_train, y_add_valid, y_add_test
+
+  # Find shuffling algorithm to rearrange the add images and labels
+  idx = np.random.permutation(len(y_train_new))
+  X_train_new_sh, y_train_new_sh = X_train_new[idx], y_train_new[idx]
+
+  idx = np.random.permutation(len(y_valid_new))
+  X_valid_new_sh, y_valid_new_sh = X_valid_new[idx], y_valid_new[idx]
+
+  idx = np.random.permutation(len(y_test_new))
+  X_test_new_sh, y_test_new_sh = X_test_new[idx], y_test_new[idx]
+
+  return  X_train_new_sh, X_valid_new_sh, X_test_new_sh, y_train_new_sh, y_valid_new_sh, y_test_new_sh
+
 
 # @brief Create model object using Keras sequential models.
 def create_seq_ANN():
@@ -55,7 +117,7 @@ def create_seq_ANN():
       keras.layers.Flatten(input_shape=[28, 28]),
       keras.layers.Dense(300, activation="relu"),
       keras.layers.Dense(100, activation="relu"),
-      keras.layers.Dense(10, activation="softmax")
+      keras.layers.Dense(11, activation="softmax")
   ])
   
   return model
@@ -95,13 +157,13 @@ def build_model():
   print(model)
 
   early_stopping_cb = keras.callbacks.EarlyStopping(patience=20)
-  checkpoint_cb = keras.callbacks.ModelCheckpoint("multiclass_MLP_v2.0.h5", save_best_only=True)
+  checkpoint_cb = keras.callbacks.ModelCheckpoint("multiclass_MLP_v2_0.h5", save_best_only=True)
 
   # Train ideal model
   train_seq_ANN(model, X_train, y_train, X_valid, y_valid, checkpoint_cb, early_stopping_cb)
 
   # saving another version of the model
-  model.save('multiclass_MLP_v2.h5')
+  model.save('multiclass_MLP_v2')
 
   # model = keras.models.load_model("my_mnist_model.h5") # rollback to best model
   print(model.evaluate(X_test, y_test))
@@ -109,6 +171,7 @@ def build_model():
 
 # @brief Grab the current version model from directory
 def get_model():
+
   X_train,  X_valid, X_test, y_train, y_valid, y_test = create_mnist_train_test()
   model = keras.models.load_model("multiclass_MLP_v1_alternate") # rollback to best model
   
